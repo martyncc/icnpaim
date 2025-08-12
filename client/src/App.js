d81bc352299983d3f589a792b1aae9a5d69d771a
Juan Luis Grandon <jota.lg19@gmail.com>
2025-08-12 14:03:03 -0400

    fix (routes): remove routes leaving only default path

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import StudentDashboard from './components/StudentDashboard';
-import AdminDashboard from './components/AdminDashboard';
-import UnitView from './components/UnitView';
+// import StudentDashboard from './components/StudentDashboard';
+// import AdminDashboard from './components/AdminDashboard';
+// import UnitView from './components/UnitView';
 import './App.css';
 
 function App() {
@@ -10,9 +10,13 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
+          {/**
+           * 
           <Route path="/student-dashboard" element={<StudentDashboard />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/unit/:unitId" element={<UnitView />} />
+           */}
+
           <Route path="/" element={<div>ICN PAIM - Accede desde Blackboard</div>} />
         </Routes>
       </div>

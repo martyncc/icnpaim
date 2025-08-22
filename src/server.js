39bc5f8cf7708d7b27d0bb3a11c0830cf51e4151
martyncc <martynccc@gmail.com>
2025-08-22 10:34:08 -0400

    ajuste rutas de inicio2

--- src/server.js
+++ src/server.js
@@ -102,7 +102,11 @@ app.all(['/lti/login/','/lti/launch/'], (req, res) => {
   const code = req.method === 'POST' ? 307 : 302;
   return res.redirect(code, target);
 });
+<<<<<<< HEAD
 app.all('/lti/login', (req, res) => {
+=======
+app.all('/lti/logi', (req, res) => {
+>>>>>>> a3ec79d (ajuste rutas de inicio)
   const qs = req.url.includes('?') ? req.url.slice(req.url.indexOf('?')) : '';
   return res.redirect(302, '/lti/login' + qs);
 });
